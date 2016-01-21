2fbb75c912ba25b5df306de41ed7374b912691f4
wenqing <yuwenqingisu@gmail.com>
2016-01-21 11:02:59 +0000

    a

--- wp-content/plugins/list-category-posts/templates/default.php
+++ wp-content/plugins/list-category-posts/templates/default.php
@@ -35,7 +35,7 @@ USA
 */
 
 /* This is the string which will gather all the information.*/
-$lcp_display_output = '';
+$lcp_display_output = '<h1>fafd</h1>';
 
 // Show category link:
 $lcp_display_output .= $this->get_category_link('strong');
