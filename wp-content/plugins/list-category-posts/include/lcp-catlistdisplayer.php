17e0c31b0e7829278dfcf43a3e850ad3e0f565c9
wenqing <yuwenqingisu@gmail.com>
2016-01-22 02:25:10 +0000

    add timeline tmaplate

--- wp-content/plugins/list-category-posts/include/lcp-catlistdisplayer.php
+++ wp-content/plugins/list-category-posts/include/lcp-catlistdisplayer.php
@@ -127,21 +127,21 @@ class CatListDisplayer {
       $lcp_display_output .= '
       <div class="cd-timeline-block">
 			<div class="cd-timeline-img cd-picture">
-				<img src="img/cd-icon-picture.svg" alt="Picture">
+				<img src="../img/cd-icon-picture.svg" alt="Picture">
 			</div> <!-- cd-timeline-img -->';
 			
 			
-		// 	// Content
-		// 	$lcp_display_output .= '
-		// 	<div class="cd-timeline-content">
-		// 		'. $this->get_post_title($post, 'h2', 'lcp_post') .'
-		// 		'. $this->get_modified_date($post) .'
-		// 		'. get_excerpt($post, 'div', 'lcp_excerpt') .'
-		// 		<a href="'. $this->get_posts_morelink($post) .'" class="cd-read-more">Read more</a>
-		// 		<span class="cd-date">' $this->get_modified_date($post) '</span>
-		// 	</div> <!-- cd-timeline-content -->
-		// </div> <!-- cd-timeline-block -->
-  //     ';
+			// Content
+			$lcp_display_output .= '
+			<div class="cd-timeline-content">
+				'. $this->get_post_title($post, 'h2', 'lcp_post') .'
+				'. $this->get_modified_date($post) .'
+				'. get_excerpt($post, 'div', 'lcp_excerpt') .'
+				<a href="'. $this->get_posts_morelink($post) .'" class="cd-read-more">Read more</a>
+				<span class="cd-date">' $this->get_modified_date($post) '</span>
+			</div> <!-- cd-timeline-content -->
+		</div> <!-- cd-timeline-block -->
+      ';
       
       
       
