945b3aac4034717970b2a8fc1dc15b7b4e74764d
wenqing <yuwenqingisu@gmail.com>
2016-01-22 01:59:22 +0000

    add template

--- wp-content/plugins/list-category-posts/include/lcp-catlistdisplayer.php
+++ wp-content/plugins/list-category-posts/include/lcp-catlistdisplayer.php
@@ -68,8 +68,8 @@ class CatListDisplayer {
     // if($tplFileName){
     //   // $this->lcp_output .= "test22";
       
-    //   echo "test1";
-    require_once TEMPLATEPATH . "/list-category-posts/" . "templates/customized.php";
+    // //   echo "test1";
+    // require_once TEMPLATEPATH . "/list-category-posts/" . "templates/customized.php";
       
     //   require_once $tplFileName;
       
@@ -79,6 +79,97 @@ class CatListDisplayer {
     // } else {
     //   $this->build_output('ul');
     // }
+    
+    
+    
+    /* This is the string which will gather all the information.*/
+    $lcp_display_output = '';
+    
+    // Show category link:
+    $lcp_display_output .= $this->get_category_link('strong');
+    
+    // Show the conditional title:
+    $lcp_display_output .= $this->get_conditional_title();
+    
+    //Add 'starting' tag. Here, I'm using an unordered list (ul) as an example:
+    $lcp_display_output .= '<ul class="lcp_catlist">test';
+    
+    /* Posts Loop
+     *
+     * The code here will be executed for every post in the category.  As
+     * you can see, the different options are being called from functions
+     * on the $this variable which is a CatListDisplayer.
+     *
+     * CatListDisplayer has a function for each field we want to show.  So
+     * you'll see get_excerpt, get_thumbnail, etc.  You can now pass an
+     * html tag as a parameter. This tag will sorround the info you want
+     * to display. You can also assign a specific CSS class to each field.
+    */
+    global $post;
+    while ( have_posts() ):
+      the_post();
+    
+      //Start a List Item for each post:
+      $lcp_display_output .= "<li>";
+    
+      //Show the title and link to the post:
+      $lcp_display_output .= $this->get_post_title($post, 'h3', 'lcp_post');
+    
+      //Show comments:
+      $lcp_display_output .= $this->get_comments($post);
+    
+      //Show date:
+      $lcp_display_output .= ' ' . $this->get_date($post);
+    
+      //Show date modified:
+      $lcp_display_output .= ' ' . $this->get_modified_date($post);
+    
+      //Show author
+      $lcp_display_output .= $this->get_author($post);
+    
+      //Custom fields:
+      $lcp_display_output .= $this->get_custom_fields($post);
+    
+      //Post Thumbnail
+      $lcp_display_output .= $this->get_thumbnail($post);
+    
+      /**
+       * Post content - Example of how to use tag and class parameters:
+       * This will produce:<p class="lcp_content">The content</p>
+       */
+      $lcp_display_output .= $this->get_content($post, 'p', 'lcp_content');
+    
+      /**
+       * Post content - Example of how to use tag and class parameters:
+       * This will produce:<div class="lcp_excerpt">The content</div>
+       */
+      $lcp_display_output .= $this->get_excerpt($post, 'div', 'lcp_excerpt');
+    
+      // Get Posts "More" link:
+      $lcp_display_output .= $this->get_posts_morelink($post);
+    
+      //Close li tag
+      $lcp_display_output .= '</li>';
+    endwhile;
+    
+    // Close the wrapper I opened at the beginning:
+    $lcp_display_output .= '</ul>';
+    
+    // If there's a "more link", show it:
+    $lcp_display_output .= $this->get_morelink();
+    
+    // Get category posts count
+    $lcp_display_output .= $this->get_category_count();
+    
+    //Pagination
+    $lcp_display_output .= $this->get_pagination();
+    
+    $this->lcp_output = $lcp_display_output;
+        
+    
+    
+    
+    
   }
 
 
