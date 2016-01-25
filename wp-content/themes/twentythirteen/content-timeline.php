dc2e7633a00985b812b4616d9692e9ac67ef13cf
wenqing <yuwenqingisu@gmail.com>
2016-01-25 03:36:29 +0000

    timeline fill real post content

--- wp-content/themes/twentythirteen/content-timeline.php
+++ wp-content/themes/twentythirteen/content-timeline.php
@@ -9,14 +9,28 @@
  * @since Twenty Thirteen 1.0
  */
 ?>
-<?php
-global $timelinePath;
-?>
+<?php global $timelinePath; ?>
 
 <?php 
 $locationIcon = "img/cd-icon-location.svg";
 $movieIcon = "img/cd-icon-movie.svg";
 $pictureIcon = "img/cd-icon-picture.svg";
+
+// category idenfication
+$category = "";
+$post_categories = wp_get_post_categories( the_ID() );
+print_r($post_categories);
+$cats = array();
+foreach($post_categories as $c){
+	$cat = get_category( $c );
+	$cats[] = array( 'name' => $cat->name, 'slug' => $cat->slug );
+	// map category
+}
+
+print_r($cats);
+				
+				
+
 ?>
 
 <div class="cd-timeline-block">
@@ -26,12 +40,7 @@ $pictureIcon = "img/cd-icon-picture.svg";
 
 			<div class="cd-timeline-content">
 				<h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
-				<p><?php 
-					$post_categories = wp_get_post_categories( the_ID() );
-					print_r($post_categories);
-				
-				
-				 ?></p>
+				<p></p>
 				<p><?php the_excerpt(); ?></p>
 				<a href="<?php the_permalink(); ?>" class="cd-read-more">Read more</a>
 				<span class="cd-date">Jan 14</span>
